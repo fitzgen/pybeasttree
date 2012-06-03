8e43bd172b873a625041fc2a699c25d10d9858a1
Nick Fitzgerald <fitzgen@gmail.com>
2012-06-03 12:41:21 -0700

    Making the id a set of ids so that we can distinguish branches which aren't end branches

--- ast.py
+++ ast.py
@@ -3,7 +3,18 @@ class Tree(object):
     def __init__(self, children = None, id = None, length = None):
         self.children = children
         self.length = length
-        self.id = id
+        self.id = set()
+
+        if children and id:
+            raise TypeError(
+                "Invalid arguments: Tree.__init__ takes either children or an id, not both")
+        elif not (children or id):
+            raise TypeError(
+                "Invalid arguments: Tree.__init__ must be called with either children or an id")
+        elif children:
+            self.id.update(*(child.id for child in self.children))
+        else: # id
+            self.id.add(id)
 
     def depth_first(self):
         pass
